7bd28eee47c133ab0630c29de6c2ca2f02c22e08
DivX <wishgood@gmail.com>
2026-05-07 20:52:11 +0800

    feat(web): add plugin route host

--- src/apps/web/src/App.tsx
+++ src/apps/web/src/App.tsx
@@ -9,10 +9,13 @@ import { AuthProvider } from './contexts/auth'
 import { ThreadListProvider } from './contexts/thread-list'
 import { AppUIProvider } from './contexts/app-ui'
 import { CreditsProvider } from './contexts/credits'
+import { BrowserTabsProvider } from './contexts/browser-tabs'
+import { PluginRuntimeProvider } from './plugins/runtime'
 import { SharePage } from './components/SharePage'
 import { VerifyEmailPage } from './components/VerifyEmailPage'
 import { OnboardingWizard } from './components/OnboardingWizard'
 import { HeadlessSetupPage } from './components/HeadlessSetupPage'
+import { PluginHostPage } from './plugins/PluginHostPage'
 import { useLocale } from './contexts/LocaleContext'
 import { shouldDelayLocalSession } from './appAuthStartup'
 import {
@@ -386,9 +389,13 @@ function App() {
             <AuthProvider accessToken={accessToken} onLoggedOut={handleLoggedOut}>
               <ThreadListProvider>
                 <AppUIProvider>
-                  <CreditsProvider>
-                    <AppLayout />
-                  </CreditsProvider>
+                  <BrowserTabsProvider>
+                    <PluginRuntimeProvider>
+                      <CreditsProvider>
+                        <AppLayout />
+                      </CreditsProvider>
+                    </PluginRuntimeProvider>
+                  </BrowserTabsProvider>
                 </AppUIProvider>
               </ThreadListProvider>
             </AuthProvider>
@@ -397,6 +404,7 @@ function App() {
             <Route path="search" element={<WelcomePage />} />
             <Route path="t/:threadId" element={<ChatShell />} />
             <Route path="t/:threadId/search" element={<ChatShell />} />
+            <Route path="plugins/:pluginId" element={<PluginHostPage />} />
             <Route path="scheduled-jobs" element={<Suspense fallback={<LoadingPage label={t.loading} />}><ScheduledJobsPage /></Suspense>} />
             <Route path="*" element={<Navigate to="/" replace />} />
           </Route>
